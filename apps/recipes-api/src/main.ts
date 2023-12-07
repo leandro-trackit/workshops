5069b46cf2c5f9404e3961fc51976f3e4442dc74
Tiago Valença <tiagovalenca@Tiagos-MacBook-Air.local>
2023-12-07 16:34:20 +0100

    Add Route

--- apps/recipes-api/src/main.ts
+++ apps/recipes-api/src/main.ts
@@ -4,11 +4,13 @@ import 'reflect-metadata';
 import cors from 'cors';
 import express from 'express';
 import { greetingsRouter } from './greetings/greetings.router';
+import { recipesRouter } from './recipes/recipes.router';
 
 const app = express();
 
 app.use(cors());
 app.use(greetingsRouter);
+app.use(recipesRouter);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
